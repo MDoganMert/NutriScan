1d94b92c6c0518b7ae5ed907fea269ebe37d9dd6
Doğan Mert <doganmert.dev@gmail.com>
2026-02-22 16:39:25 +0300

    Initial Commit

---
+++ NutriScan/NutriScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriScanApp.swift
+//  NutriScan
+//
+//  Created by Mehmet Dogan Mert on 22.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct NutriScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
